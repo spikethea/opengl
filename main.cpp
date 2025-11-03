ad113861458ec5b9605b5c433752539d35ebd0c3
QuinceGR <57224107+spikethea@users.noreply.github.com>
2025-11-03 23:39:28 +0000

    Project setup, Linking Libraries Headers & GLEW replaced for GLAD

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 using namespace std;
-#include <GL/glew.h>
+//#include <GL/glew.h>
+#include <glad/glad.h> 
 #include <GLFW/glfw3.h>
+
 
 /*resize the viewport to the window when the user resizes*/
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
@@ -18,30 +20,42 @@ void processInput(GLFWwindow *window)
 
 int main(void)
 {
+    std::cout << "Hello World" << std::endl;
     GLFWwindow* window;
 
-    /* Initialize the library */
-    if (!glfwInit())    
-        return -1;
-
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(800, 600, "Hello World", NULL, NULL);
     if (!window)
     {
+        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
 
+
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    // Initialise GLAD before calling any OpenGL Function
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
     glViewport(0, 0, 800, 600);
     
-    if(glewInit() != GLEW_OK){
-        cout << "Unable to load glew" << endl;
-    }
-    else{
-        cout << glGetString(GL_VERSION) << endl;
-    }
+   
+    //if(glewInit() != GLEW_OK){
+    //    cout << "Unable to load glew" << endl;
+    //}
+    //else{
+    //    cout << glGetString(GL_VERSION) << endl;
+    //}
 
    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
